01683bf7f1f1fac8059b59aaf93eae9419fab242
Renato Neto <rnato.netoo@gmail.com>
2024-04-21 01:36:29 -0300

    client predicted input

--- client/index.ts
+++ client/index.ts
@@ -18,6 +18,9 @@ export class GameScene extends Phaser.Scene {
 
   cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
 
+  currentPlayer: Phaser.Types.Physics.Arcade.ImageWithDynamicBody;
+  remoteRef: Phaser.GameObjects.Rectangle;
+
   preload() {
     this.load.image('ship_0001', 'https://cdn.glitch.global/3e033dcd-d5be-4db4-99e8-086ae90969ec/ship_0001.png');
     this.cursorKeys = this.input.keyboard.createCursorKeys();
@@ -34,18 +37,23 @@ export class GameScene extends Phaser.Scene {
         const entity = this.physics.add.image(player.x, player.y, 'ship_0001')
         this.playerEntities[sessionId] = entity
 
-        // listening for server updates
-        player.onChange(() => {
-          //
-          // do not update local position immediately
-          // we're going to LERP them during the render loop.
-          //
-          entity.setData('serverX', player.x)
-          entity.setData('serverY', player.y)
-        })
-        // Alternative, listening to individual properties:
-        // player.listen("x", (newX, prevX) => console.log(newX, prevX));
-        // player.listen("y", (newY, prevY) => console.log(newY, prevY));
+        if (sessionId === this.room.sessionId) {
+          this.currentPlayer = entity
+          // remoteRef is being used got debug only
+          this.remoteRef = this.add.rectangle(0, 0, entity.width, entity.height);
+          this.remoteRef.setStrokeStyle(1, 0xff0000);
+
+          player.onChange(() => {
+            this.remoteRef.x = player.x;
+            this.remoteRef.y = player.y;
+          })
+        } else {
+          // interpolate remote players
+          player.onChange(() => {
+            entity.setData('serverX', player.x);
+            entity.setData('serverY', player.y);
+          });
+        }
       });
 
       this.room.state.players.onRemove((player, sessionId) => {
@@ -65,14 +73,32 @@ export class GameScene extends Phaser.Scene {
     if (!this.room) { return; }
 
     // send input to the server
+    const velocity = 2;
     this.inputPayload.left = this.cursorKeys.left.isDown
     this.inputPayload.right = this.cursorKeys.right.isDown
     this.inputPayload.up = this.cursorKeys.up.isDown
     this.inputPayload.down = this.cursorKeys.down.isDown
     this.room.send(0, this.inputPayload)
 
+    if (this.inputPayload.left) {
+      this.currentPlayer.x -= velocity;
+
+    } else if (this.inputPayload.right) {
+      this.currentPlayer.x += velocity;
+    }
+
+    if (this.inputPayload.up) {
+      this.currentPlayer.y -= velocity;
+
+    } else if (this.inputPayload.down) {
+      this.currentPlayer.y += velocity;
+    }
+
     for (let sessionId in this.playerEntities) {
-      // interpolate all players entities
+      // do not interpolate the current player
+      if (sessionId === this.room.sessionId) { continue; }
+
+      // interpolate all other players entities
       const entity = this.playerEntities[sessionId]
       const { serverX, serverY } = entity.data.values
 
